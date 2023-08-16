4b3e4aab017e12e61b89095f5f6a45d9ed794610
Reena A. Alandkar <alandkar@princeton.edu>
2023-08-15 23:20:01 -0400

    first commit

---
+++ src/test/java/com/example/Cashcard/CashcardJsonTest.java
@@ -0,0 +1,13 @@
+package com.example.Cashcard;
+
+
+import org.junit.jupiter.api.Test;
+import static
+        org.assertj.core.api.Assertions.assertThat;
+
+public class CashcardJsonTest {
+    @Test
+    public static void myfirsttest(){
+        assertThat(42).isEqualTo(42);
+    }
+}
